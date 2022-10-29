485bddeab81dcef02c24e9655647a00ee2897475
Edvaldo Machado dos Santos <edvaldo.santos@docebela.local>
2022-10-29 12:08:02 -0300

    Aula 01 Primeiro

---
+++ Primeiro.java
@@ -0,0 +1,21 @@
+public class Primeiro {
+	//Classes Wrapper
+	//int		= Integer
+	//float		= Float
+	//boolean	= Boolean
+	//			= String
+	public static void main(String [] args) {
+		
+		String nome = args[0];
+		
+		String sobrenome = args[1];
+		
+		int idade = Integer.valueOf(args[2]);
+	
+		// Utilizando o parâmetro args para passagem de valor para idade
+		// System.out.println(nome + " " + sobrenome + " " + args[2]);	
+		
+		// Utilzando a variavel idade diretamente
+		System.out.println(nome + " " + sobrenome + " " + idade);		
+	}
+}
