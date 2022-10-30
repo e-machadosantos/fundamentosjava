2000aa4ac210b75f10f0ae744e3ab0ae50fd8dc8
Edvaldo Machado <edvaldo.santos@al.infnet.edu.br>
2022-10-30 12:54:40 -0300

    Segundo com for each

---
+++ Segundo.java
@@ -0,0 +1,11 @@
+public class Segundo {
+	public static void main(String [] args) {
+		
+		// usanto for eatch
+		// for(tipoColecao obj : nomeColecao)
+		
+	for(String nome : args) {
+			System.out.println(nome);
+		}
+	}
+}
