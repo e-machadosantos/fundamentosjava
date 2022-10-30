49affecbabbc0376e00bdfb4b280a2e70a2ff1ac
Edvaldo Machado <edvaldo.santos@al.infnet.edu.br>
2022-10-30 14:43:09 -0300

    Segundo com Scanner v1

--- Segundo.java
+++ Segundo.java
@@ -5,18 +5,11 @@ public class Segundo {
 		
 		Scanner in = new Scanner(System.in);
 		
-		String nome = in.next();
-		
-		int idade = in.nextInt();
-		
+		String nome = in.next();		
+		int idade = in.nextInt();		
 		float salario = in.nextFloat();
 		
 		System.out.println(nome + " - " + idade + " - " + salario);
-		int a = 0;
-		do {
-			System.out.println(args[a]);			
-			a++;
-		} while (a < args.length);
 		
 		in.close();
 	}
