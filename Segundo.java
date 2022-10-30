621079662dafc53cf1f50a510b64587bb08d5a43
Edvaldo Machado <edvaldo.santos@al.infnet.edu.br>
2022-10-30 13:12:36 -0300

    Segundo com While

--- Segundo.java
+++ Segundo.java
@@ -1,11 +1,17 @@
 public class Segundo {
 	public static void main(String [] args) {
 		
-		// usanto for tradicional
-		// for(inicializacao; condicao; incremento)
+		// usanto While
+		// inicializacao
+		// while(condicao) {
+		//		incremento
+		// }
 		
-	for(int i = 0; i < args.length; i++) {
-			System.out.println(args[i]);
+		int a = 0;
+		while(a < args.length) {
+			System.out.println(args[a]);
+		
+			a++;
 		}
 	}
 }
